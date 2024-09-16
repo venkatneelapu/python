87a86c6d31cef1dbad909e453e726e94370f4fd8
Neelapu Venkata Naidu <114356828+venkatneelapu@users.noreply.github.com>
2024-09-16 21:30:30 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,15 @@
+def print_number_5():
+    pattern = [
+        "*****",
+        "*    ",
+        "*    ",
+        "*****",
+        "    *",
+        "    *",
+        "*****"
+    ]
+    
+    for line in pattern:
+        print(line)
+
+print_number_5()
