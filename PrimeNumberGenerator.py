b0868e545654e4e4318aa9c8da270a580d048921
Neelapu Venkata Naidu <114356828+venkatneelapu@users.noreply.github.com>
2024-09-25 23:08:23 +0530

    Create PrimeNumberGenerator.py

---
+++ PrimeNumberGenerator.py
@@ -0,0 +1,17 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def generate_primes(limit):
+    primes = []
+    for num in range(2, limit + 1):
+        if is_prime(num):
+            primes.append(num)
+    return primes
+
+limit = 100  # Set the limit for prime numbers
+print(generate_primes(limit))
